73926f450b87364a406a737234f8873c5975f8f9
Damianofds <damianofds@gmail.com>
2016-05-28 14:42:46 +0200

    fixed claim loading

--- lightmap/lightmap.js
+++ lightmap/lightmap.js
@@ -1,86 +1,89 @@
-var mapConfig = jsonMap;
-mapConfig.layers;
+$( document ).ready(loadMap);
 
-if(!validateMapObject(mapConfig.map)){
-    alert("Something went badly wrong! Check the console for more info");
-    throw new Error("Error in the Map configuration - check your mapConfig.json");
-}
+function loadMap(){
+    var mapConfig = jsonMap;
+    mapConfig.layers;
 
-if(!validateLayersObject(mapConfig.layers)){
-    alert("Something went badly wrong! Check the console for more info");
-    throw new Error("Error in the Layers configuration - check your mapConfig.json");
-}
-
-/**
-    Parse the map configuration:
-    - Create WMS or TMS layers
-    - identify baseMaps, overlayMaps, activelayers or var timeseries
-*/
-var baseMaps = [];
-var overlayMaps = [];
-var activelayers = [];
-var timeseries = [];
-
-var layers = mapConfig.layers;
-for(var i=0; i<layers.length; i++){
-    var lyr = layers[i];
-    var tmpLayer;
-    if(lyr.type == "TMS"){
-        tmpLayer = L.tileLayer(lyr.url,{
-            attribution: lyr.attribution
-        })
+    if(!validateMapObject(mapConfig.map)){
+        alert("Something went badly wrong! Check the console for more info");
+        throw new Error("Error in the Map configuration - check your mapConfig.json");
     }
-    if(lyr.type == "WMS"){
-        if(lyr.timedimension){
-            tmpLayer = L.tileLayer.wms(mapConfig.map[0].wmsSource, {
-                layers: lyr.layerName,
-                transparent: true,
-                format: 'image/png8',
-                time: '2001-01-01T00:00:00.000Z'
-            });
-        }else{
-            tmpLayer = L.tileLayer.wms(mapConfig.map[0].wmsSource, {
-                layers: lyr.layerName,
-                format: lyr.format,
-                transparent: lyr.transparent,
+
+    if(!validateLayersObject(mapConfig.layers)){
+        alert("Something went badly wrong! Check the console for more info");
+        throw new Error("Error in the Layers configuration - check your mapConfig.json");
+    }
+
+    /**
+        Parse the map configuration:
+        - Create WMS or TMS layers
+        - identify baseMaps, overlayMaps, activelayers or var timeseries
+    */
+    var baseMaps = [];
+    var overlayMaps = [];
+    var activelayers = [];
+    var timeseries = [];
+
+    var layers = mapConfig.layers;
+    for(var i=0; i<layers.length; i++){
+        var lyr = layers[i];
+        var tmpLayer;
+        if(lyr.type == "TMS"){
+            tmpLayer = L.tileLayer(lyr.url,{
                 attribution: lyr.attribution
-            });
+            })
+        }
+        if(lyr.type == "WMS"){
+            if(lyr.timedimension){
+                tmpLayer = L.tileLayer.wms(mapConfig.map[0].wmsSource, {
+                    layers: lyr.layerName,
+                    transparent: true,
+                    format: 'image/png8',
+                    time: '2001-01-01T00:00:00.000Z'
+                });
+            }else{
+                tmpLayer = L.tileLayer.wms(mapConfig.map[0].wmsSource, {
+                    layers: lyr.layerName,
+                    format: lyr.format,
+                    transparent: lyr.transparent,
+                    attribution: lyr.attribution
+                });
+            }
+        }
+        if(lyr.basemap){
+            baseMaps[lyr.label] = tmpLayer;
+        }else{
+            overlayMaps[lyr.label] = tmpLayer;
+        }
+        if(lyr.active){
+            activelayers.push(tmpLayer);
+        }
+        if(lyr.timedimension){
+            timeseries.push(tmpLayer);
         }
     }
-    if(lyr.basemap){
-        baseMaps[lyr.label] = tmpLayer;
-    }else{
-        overlayMaps[lyr.label] = tmpLayer;
-    }
-    if(lyr.active){
-        activelayers.push(tmpLayer);
-    }
-    if(lyr.timedimension){
-        timeseries.push(tmpLayer);
-    }
+
+
+    /**
+        Build the map object as configured in the map configuration
+    */
+    var map;
+    map = L.map('map', {
+            center: mapConfig.map[0].center,
+            zoom: mapConfig.map[0].zoom,
+            layers: activelayers
+        });
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+    var sliderControl = L.control.sliderControl({
+        position: 'topright', 
+        layers: timeseries,
+        startTime: '2001-01-01T00:00:00.000Z',
+        endTime: '2014-01-01T00:00:00.000Z',
+        timeStep: 60*60*24*368,
+        range: true}).addTo(map);
+    sliderControl.startSlider();
 }
-
-
-/**
-    Build the map object as configured in the map configuration
-*/
-var map;
-map = L.map('map', {
-        center: mapConfig.map[0].center,
-        zoom: mapConfig.map[0].zoom,
-        layers: activelayers
-    });
-L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-var sliderControl = L.control.sliderControl({
-    position: 'topright', 
-    layers: timeseries,
-    startTime: '2001-01-01T00:00:00.000Z',
-    endTime: '2014-01-01T00:00:00.000Z',
-    timeStep: 60*60*24*368,
-    range: true}).addTo(map);
-sliderControl.startSlider();
-
 /**
     Helper functions
 */
