949fa0ab4946dc2629afa2e2af31104e832f9e6b
Damiano Giampaoli <damianofds@gmail.com>
2017-04-09 11:05:07 +0200

    Update lightmap.js

--- lucca-ages/lightmap.js
+++ lucca-ages/lightmap.js
@@ -78,7 +78,7 @@ function loadMap(){
         position: 'topright', 
         layers: timeseries,
         startTime: '1',
-        endTime: '2016',
+        endTime: '2015',
         timeStep: 60*60*24*368*500,
         range: true}).addTo(map);
     sliderControl.startSlider();
